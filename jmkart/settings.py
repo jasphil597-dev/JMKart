3511c66f40600190f9f79a9a66a326d3855d9ceb
Joemer <joemers97@gmail.com>
2025-10-20 13:06:31 -0400

    feat: complete media configuration and slug auto-population

--- jmkart/settings.py
+++ jmkart/settings.py
@@ -124,6 +124,13 @@ STATICFILES_DIRS = [
     'jmkart/static',
 ]
 
+# media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
